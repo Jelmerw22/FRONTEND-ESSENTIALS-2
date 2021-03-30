da63f7dcfb9f825d63032f71bf0bd0f9ab140ad9
Jelmer Wiersma <jelmer.wiersma@student.rocva.nl>
2021-03-30 12:17:18 +0200

    java return

---
+++ 02-Javascript/taak02-Functions-introductie/js/script.js
@@ -0,0 +1,11 @@
+alert("Hallo, dit is een willekeurige boodschap!");
+
+promtLog = prompt("Voer even wat in SVP.");
+console.log(promtLog);
+
+confirmLog = confirm("OK is oke en cancel is dan niet!");
+if(confirmLog == true){
+    console.log("OK")
+} else{
+    console.log("Dan niet!")
+}
