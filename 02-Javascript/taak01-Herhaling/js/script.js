aafeb384158776703b753c8fef26b4e6f9890984
Jelmer Wiersma <jelmer.wiersma@student.rocva.nl>
2021-03-30 11:47:52 +0200

    java if else if

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,2 +1,14 @@
-let strNaam;
-let boolNaamKlopt;
+strNaam = prompt("Hoe heet je?");
+boolNaamKlopt = confirm( "Heet je echt " + strNaam + "?");
+
+if(boolNaamKlopt == true){
+    alert("Welkom!");
+}else{
+    strNaam = prompt("Nog een keer; Hoe heet je?");
+    boolNaamKlopt = confirm( "Heet je echt " + strNaam + "?");
+        if(boolNaamKlopt == true){
+            alert("Deze keer wel? Welkom!")
+        }else{
+            alert("Alsnog Welkom!")
+        }
+}
