0c893ccd29b503783e397613375b02aa52fccdd5
Jelmer Wiersma <jelmer.wiersma@student.rocva.nl>
2021-04-01 14:35:09 +0200

    opdrachten

--- 02-Javascript/taak06-Functions-Samenvatting/js/script.js
+++ 02-Javascript/taak06-Functions-Samenvatting/js/script.js
@@ -11,10 +11,40 @@ function showRandomNumber1(){
     return randomGetal;
 }
 
-getal1 = showRandomNumber1();
-
-function geefAntwoord(){
-    if(getal1 >> getal2)
-        { document.body.style.backgroundColor="green"
-            }else{document.body.style.backgroundColor="red"}
+function showRandomNumber2(){
+    let randomGetal = makeRandomNumber()
+    document.querySelector(".getal2").innerHTML = randomGetal;
+    return randomGetal;
 }
+ 
+
+function geefAntwoord(antwoord){
+    var score = 0;
+    getal1 = document.querySelector(".getal1").innerHTML;
+    getal2 = document.querySelector(".getal2").innerHTML;
+    score = document.querySelector(".score").innerHTML;
+if (getal1 > getal2 && antwoord == 'hoger'){
+    score = score -1 + 2;
+    document.querySelector(".score").innerHTML = score;
+    document.querySelector("body").style.backgroundColor = "green";
+    }
+    else if(getal1 < getal2 && antwoord == 'lager'){
+        score = score -1 +2;
+        document.querySelector(".score").innerHTML = score;
+        document.querySelector("body").style.backgroundColor = "green";
+    }
+    if (getal1 < getal2 && antwoord == 'hoger'){
+        score = score -1;
+        document.querySelector(".score").innerHTML = score;
+        document.querySelector("body").style.backgroundColor = "red";
+        }
+        else if(getal1 > getal2 && antwoord == 'lager'){
+            score = score -1;
+            document.querySelector(".score").innerHTML = score;
+            document.querySelector("body").style.backgroundColor = "red";
+        }
+        showRandomNumber1();
+        showRandomNumber2();
+    }
+    showRandomNumber1();
+    showRandomNumber2();
